9df423e3c52f83a61e5a5204e2205d3dae013ed5
Eugen Jercan <jercan.ionut@gmail.com>
2017-03-14 07:14:09 +0200

    Added validation for ex01 php script.

--- ex01/sayhello.php
+++ ex01/sayhello.php
@@ -1,3 +1,6 @@
 <?php
-    $name = $_GET['name'];
-    echo 'Hello ' . $name . '!';
+    if (empty($_GET['name']) || is_numeric($_GET['name'])) {
+        echo "<p>You didn't enter a valid name!</p>";
+    } else {
+        echo '<p>Hello ' . $_GET['name'] . '!</p>';
+    }
